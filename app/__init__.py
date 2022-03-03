e9731669c9535c0f182a373f24becce9b3d31916
derekcodingtemple <derekh@codingtemple.com>
2022-03-03 14:08:38 -0600

    update

--- app/__init__.py
+++ app/__init__.py
@@ -27,15 +27,16 @@ def create_app(config_class=Config):
     from app.blueprints.blog import bp as blog
     from app.blueprints.main import bp as main
     from app.blueprints.authentication import bp as authentication
-    from app.blueprints.api import bp as api
     app.register_blueprint(blog)
     app.register_blueprint(main)
-    app.register_blueprint(api)
     app.register_blueprint(authentication)
 
     with app.app_context():
         from app.blueprints.ecommerce import bp as ecommerce
         app.register_blueprint(ecommerce)
+
+        from app.blueprints.api import bp as api
+        app.register_blueprint(api)
         # from app.blueprints.ecommerce import routes
 
     return app
